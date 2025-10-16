ed61daa4e907fb1e022dc0176218664cb4c66f12
Gurleen <grlnsngh@gmail.com>
2025-10-15 23:25:13 -0400

    refactor: update package name to 'portfolio' and add @vercel/analytics for improved analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Portfolio",
@@ -91,6 +92,7 @@ export default function RootLayout({
           <div className="relative flex">{children}</div>
           <Toaster />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
